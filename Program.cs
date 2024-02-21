d18d6e4a819ff44d54d4ffa48d14360262238642
adamishakad <155333634+adamishakad@users.noreply.github.com>
2024-02-20 18:19:24 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Loops_Adam_022024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
